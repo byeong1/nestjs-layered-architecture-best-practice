61dc3cfd3d7cc14994f97127a7418424a3d30c56
byeong1 <qoquddlf00@gmail.com>
2026-02-01 01:49:23 +0900

    feat(common): 미들웨어 추가
    - BaseMiddleware: 미들웨어 기본 클래스

---
+++ src/common/middlewares/base.middleware.ts
@@ -0,0 +1,14 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { Request, Response, NextFunction } from "express";
+
+
+/**
+ * base middleware for all middlewares
+ * 
+ * @description
+ * This middleware is used to test the base route
+ */
+@Injectable()
+export abstract class BaseMiddleware implements NestMiddleware {
+    public abstract use(req: Request, res: Response, next: NextFunction): void;
+}
