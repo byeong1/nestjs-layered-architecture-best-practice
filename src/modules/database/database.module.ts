ea1bd36ed4bc8ccfc5454f653523a7b8d8a83e8e
byeong1 <qoquddlf00@gmail.com>
2026-02-01 01:49:44 +0900

    feat(database): 데이터베이스 모듈 추가
    - DatabaseModule: TypeORM 데이터베이스 연결 모듈

---
+++ src/modules/database/database.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '../config/config.service';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            useFactory: (configService: ConfigService) => configService.databaseConfig,
+            inject: [ConfigService],
+        }),
+    ],
+})
+export class DatabaseModule {}
